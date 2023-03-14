b57b0f44dd898fb3deedffc48ead0db9751fedd5
uhdznataly <112514096+uhdznataly@users.noreply.github.com>
2023-03-14 15:42:49 -0500

    add .status to router.use

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
-router.use((req, res) => res.send('Wrong route!'));
+router.use((req, res) => res.status(404).send('Wrong route!'));
 
 module.exports = router;
