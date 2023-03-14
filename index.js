58fa413b9a7943776cff981bf8de5aff7d1e101b
uhdznataly <112514096+uhdznataly@users.noreply.github.com>
2023-03-14 15:39:42 -0500

    add mongoose require to index.js

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const db = require('./config/connection');
-const routes = require('./routes');
+const mongoose = require('mongoose');
 
-const PORT = 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-API', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.set('debug', true);
 
 db.once('open', () => {
   app.listen(PORT, () => {
